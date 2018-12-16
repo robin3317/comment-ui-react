b7c598ea5bc01ec2bd5999945c4090be8ad9d186
robin3317 <abdurrahmanrobin47@gmail.com>
2018-12-17 01:46:59 +0600

    faker package is added and comment is ready as hard coded

--- src/index.js
+++ src/index.js
@@ -1,23 +1,59 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 const App = () => {
  return(
-   <div className="ui container comments">
-     <div className="comment">
-       <a href="/" className="avatar">
-         <img alt="avatar" />
+   <div className='ui container comments'>
+
+     {/*-- first comment --*/}
+     <div className='comment'>
+       <a href='/' className='avatar'>
+         <img alt='avatar' src={faker.image.avatar()} />
        </a>
-       <div className="content">
-         <a href="/" className="author">
+       <div className='content'>
+         <a href='/' className='author'>
            Sam
          </a>
+         <div className='metadata'>
+           <span className='date'>Today at 6:00PM</span>
+         </div>
+         <div className='text'>Nice blog post</div>
        </div>
-       <div className="metadata">
-         <span className="date">Today at 6:00PM</span>
-       </div>
-       <div className="text">Nice blog post</div>
      </div>
+
+     {/*-- second comment --*/}
+     <div className='comment'>
+       <a href='/' className='avatar'>
+         <img alt='avatar' src={faker.image.avatar()} />
+       </a>
+       <div className='content'>
+         <a href='/' className='author'>
+           Sam
+         </a>
+         <div className='metadata'>
+           <span className='date'>Today at 6:00PM</span>
+         </div>
+         <div className='text'>Nice blog post</div>
+       </div>
+     </div>
+
+     {/*-- third comment --*/}
+     <div className='comment'>
+       <a href='/' className='avatar'>
+         <img alt='avatar' src={faker.image.avatar()} />
+       </a>
+       <div className='content'>
+         <a href='/' className='author'>
+           Sam
+         </a>
+         <div className='metadata'>
+           <span className='date'>Today at 6:00PM</span>
+         </div>
+         <div className='text'>Nice blog post</div>
+       </div>
+     </div>
+
    </div>
  );
 };
